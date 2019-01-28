2bed79bb1350697815a3ad8b5aacc0d08a20dd19
Tianyu Cui <2397867c@student.gla.ac.uk>
2019-01-28 22:00:46 +0000

    5.7

--- rango/models.py
+++ rango/models.py
@@ -3,6 +3,9 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(max_length=128, unique=True)
 
+    class Meta:
+        verbose_name_plural = 'Categories'
+
     def __str__(self): # For Python 2, use __unicode__ too
         return self.name
 
@@ -12,5 +15,5 @@ class Page(models.Model):
     url = models.URLField()
     views = models.IntegerField(default=0)
 
-    def __str__(self): # For Python 2, use __unicode__ too
+    def __str__(self):
         return self.title
